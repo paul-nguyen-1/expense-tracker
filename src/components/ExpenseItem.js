e0f9c50a7c1ae161a2631d3f9adf5be2d87f9c74
paul-nguyen96 <paul121596@icloud.com>
2022-12-23 23:06:15 -0600

    import data and props for expenseitem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,13 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-const expenseDate = new Date(2022, 12, 15);
-const expenseTitle = 'Tuition';
-const expenseAmount = 4500.73;
-
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toDateString()}</div>
+      <div>{props.date.toDateString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">${expenseAmount}</div>
+      <div className="expense-item__price">${props.amount}</div>
     </div>
   );
 }
