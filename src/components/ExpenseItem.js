d9db38e5e0ff0926d7f53060dddcd0917d0b829b
paul-nguyen96 <paul121596@icloud.com>
2023-01-17 00:40:19 -0600

    set up expense form and expenses

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react'
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+  let date = props.date;
+  let amount = props.amount;
+
+//Click Handler
+const [title, setTitle ] = useState(props.title)
+
+  const clickHandler = () => {
+    setTitle('Updated!')
+  };
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date} />
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
-      <div className="expense-item__price">${props.amount}</div>
+      <div className="expense-item__price">${amount}</div>
+      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
